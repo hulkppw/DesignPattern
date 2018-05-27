cd0831459de4b70d18f5829e48234093e48e73e9
hulkppw <hulkppw@gmail.com>
2018-05-27 19:09:35 +0800

    chain

---
+++ DesignPattern/src/main/java/v2/chain/FileLogger.java
@@ -0,0 +1,12 @@
+package v2.chain;
+
+
+public class FileLogger extends AbstractLogger{
+    public FileLogger(int level){
+        this.level = level;
+    }
+    @Override
+    protected void write(String message) {
+        System.out.println("File::Logger: " + message);
+    }
+}
